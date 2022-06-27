5ea1a573454531196a9941d3d19e3e3d496f304e
jinnyfruit <jinnyfruit0527@gmail.com>
2022-06-27 15:35:25 +0900

    linear Regression code

--- Regression/linearRegression.py
+++ Regression/linearRegression.py
@@ -3,6 +3,7 @@ file name: Linear Regression
 modified: 2022.06.27
 author: jinnyfruit
 '''
+
 import numpy as np
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
